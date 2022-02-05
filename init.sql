db96c2bb62cfc500273d42465e571e977dae58be
skoc <skoc10ds@gmail.com>
2022-02-05 14:43:44 +0300

    complete

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE phonebook_db.phonebook(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    number VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO phonebook_db.phonebook (name, number)
+    VALUES
+        ("skoc", "12345673"),
+        ("susta", "6785445"),
+        ("bayram", "876543554");
